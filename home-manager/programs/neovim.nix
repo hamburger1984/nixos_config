76a44e44c3afa2d9632b712ae4c45294ca463c2d
Andreas <hamburger1984@gmail.com>
2022-12-14 21:12:25 +0100

    neovim: this might fix the error on startup .. treesitter needs a writable directory

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -512,7 +512,14 @@ in
       "-------------------------------------------------------------------------------
       " see https://github.com/nvim-treesitter/nvim-treesitter
       lua << EOF
+      -- Defines a read-write directory for treesitters in nvim's cache dir
+      local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+      vim.fn.mkdir(parser_install_dir, "p")
+
       require('nvim-treesitter.configs').setup {
+        -- ...
+        parser_install_dir = parser_install_dir
+
         -- one of "all" or a list of languages
         ensure_installed = "all",
         -- ensure_installed = { "bash", "c", "c_sharp", "cpp", "css", "go",
