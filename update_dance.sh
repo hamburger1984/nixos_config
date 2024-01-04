824ca0ed1ed4bbc4b3c48afb89baa08b8a56fe8d
Andreas <hamburger1984@gmail.com>
2024-01-04 16:50:45 +0100

    let's also rebuild

--- update_dance.sh
+++ update_dance.sh
@@ -4,3 +4,7 @@ pushd config; git pull; popd
 pushd nixpkgs; git checkout master; git pull; git checkout -; git rebase master; popd
 
 pushd plasma-manager; git pull; popd
+
+sudo nix-channel --update
+
+sudo nixos-rebuild -I nixpkgs=/home/andreas/Projects/nixos-related/nixpkgs switch --upgrade
