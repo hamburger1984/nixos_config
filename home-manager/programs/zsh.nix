4a7706489d0bc675861a4870e0c0b8af943a1e5e
Andreas K <hamburger1984@gmail.com>
2024-10-19 16:32:42 +0200

    zshing..

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -12,7 +12,9 @@
 
     autosuggestion = {
       enable = true;
+      strategy = ["history" "completion"];
     };
+
     syntaxHighlighting.enable = true;
 
     historySubstringSearch.enable = true;
@@ -26,74 +28,23 @@
 
     defaultKeymap = "viins";
 
+    dotDir = ".config/zsh";
+
     sessionVariables = {
       PATH = "$PATH:/home/andreas/.dotnet/tools:/home/andreas/.local/bin:/home/andreas/.local/share/JetBrains/Toolbox/scripts";
       FREETYPE_PROPERTIES = "cff:no-stem-darkening=0";
     };
-
-    #shellOptions = [
-    #  # Append to history file rather than replacing it.
-    #  "histappend"
-
-    #  # Save multi-line commands as one command
-    #  "cmdhist"
-
-    #  # check the window size after each command and, if
-    #  # necessary, update the values of LINES and COLUMNS.
-    #  "checkwinsize"
-
-    #  # Extended globbing.
-    #  "extglob"
-    #  "globstar"
-
-    #  # Case-insensitive globbing (used in pathname expansion)
-    #  "nocaseglob"
-
-    #  # Prepend cd to directory names automatically
-    #  "autocd"
-    #  # Correct spelling errors during tab-completion
-    #  "dirspell"
-    #  # Correct spelling errors in arguments supplied to cd
-    #  "cdspell"
-
-    #  # Warn if closing shell with running jobs.
-    #  "checkjobs"
-    #];
-
-
-    #initExtra = ''
-    #  # Prevent file overwrite on stdout redirection
-    #  # Use `>|` to force redirection to an existing file
-    #  set -o noclobber
-
-    #  # Enable history expansion with space
-    #  # E.g. typing !!<space> will replace the !! with your last command
-    #  bind Space:magic-space
-
-    #  # Perform file completion in a case insensitive fashion
-    #  bind "set completion-ignore-case on"
-
-    #  # Treat hyphens and underscores as equivalent
-    #  bind "set completion-map-case on"
-
-    #  # Display matches for ambiguous patterns at first tab press
-    #  bind "set show-all-if-ambiguous on"
-
-    #  # Immediately add a trailing slash when autocompleting symlinks to directories
-    #  bind "set mark-symlinked-directories on"
-
-    #  # Enable incremental history search with up/down arrows (also Readline goodness)
-    #  # Learn more about this here: http://codeinthehole.com/writing/the-most-important-command-line-tip-incremental-history-searching-with-inputrc/
-    #  bind '"\e[A": history-search-backward'
-    #  bind '"\e[B": history-search-forward'
-    #  bind '"\e[C": forward-char'
-    #  bind '"\e[D": backward-char'
-    #'';
   };
 
   programs.mcfly = {
     enable = true;
     enableZshIntegration = true;
+
+    # Whether to enable fuzzy searching.
+    # 0 is off; higher numbers weight toward shorter matches.
+    # Values in the 2-5 range get good results so far.
+    fuzzySearchFactor = 3;
+
     fzf.enable = true;
   };
 }
